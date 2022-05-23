50074c6dca08e4e75a50688c816cb6fcf36d9909
sergiokb <94998660+sergiokb@users.noreply.github.com>
2022-05-23 06:05:33 +0300

    Update cycle_search.cpp

--- cycle_search.cpp
+++ cycle_search.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <string>
+bool cycle_search(int n, std::vector<std::vector<int>>& sons, std::vector<int>& cycle);
 void dfs(int v, int p, std::vector<std::string>& color, bool& isCycle,
          std::vector<int>& parent, std::vector<std::vector<int>>& sons, std::vector<int>& cycle);
 int main() {
     std::ios_base::sync_with_stdio(false);
     int n, m, a, b;
-    bool isCycle = false;
-    std::vector<int>cycle;
     std::cin >> n >> m;
-    std::vector<std::string>color(n, "WHITE");
     std::vector<std::vector<int>>sons(n);
-    std::vector<int>parent(n, -1);
     for(int i = 0; i < m; i++) {
         std::cin >> a >> b;
         --a;
         --b;
         sons[a].push_back(b);
     }
-    for(int i = 0; i < n; i++) {
-        if(color[i] == "WHITE") dfs(i, -1, color, isCycle, parent, sons, cycle);
-    }
-    if(!isCycle) std::cout << "NO";
-    else {
+    std::vector<int>cycle;
+    if(cycle_search(n, sons, cycle)) {
         std::cout << "YES" << '\n';
-        for(int i = cycle.size() - 1; i >= 0; i--) std::cout << cycle[i] + 1 << ' ';
+        for(int i = int(cycle.size()) - 1; i >= 0; i--)
+            std::cout << cycle[i] + 1 << ' ';
     }
+    else std::cout << "NO" << '\n';
     return 0;
+}
+
+bool cycle_search(int n, std::vector<std::vector<int>>& sons, std::vector<int>& cycle) {
+    bool isCycle = false;
+    std::vector<std::string>color(n, "WHITE");
+    std::vector<int>parent(n, -1);
+    for(int i = 0; i < n; i++) {
+        if(color[i] == "WHITE")
+            dfs(i, -1, color, isCycle, parent, sons, cycle);
+    }
+    if(isCycle) return true;
+    return false;
 }
 
 void dfs(int v, int p, std::vector<std::string>& color, bool& isCycle,
