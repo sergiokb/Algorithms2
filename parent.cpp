7be01489fe2cd6f0b62daabe3c6e25bcc72ae503
sergiokb <94998660+sergiokb@users.noreply.github.com>
2022-05-23 05:56:17 +0300

    Update parent.cpp

--- parent.cpp
+++ parent.cpp
@@ -1,32 +1,51 @@
 #include <iostream>
 #include <vector>
+void solution(int n, int m, std::vector<int>& ans,
+              std::vector<int>& input, std::vector<std::pair<int, int>>& pairs);
 void dfs(int v, int& timer, std::vector<int>& tin, std::vector<int>& tout,
          std::vector<int>& parent, std::vector<std::vector<int>>& sons);
 int main() {
     std::ios_base::sync_with_stdio(false);
-    int n, m, father, a, b;
-    int timer = 0;
+    int n, m;
     std::cin >> n;
+    std::vector<int>input(n);
+    for(int i = 0; i < n; i++) {
+        std::cin >> input[i];
+    }
+    std::cin >> m;
+    std::vector<std::pair<int, int>>pairs(m);
+    for(int i = 0; i < m; i++) {
+        std::cin >> pairs[i].first >> pairs[i].second;
+    }
+    std::vector<int>ans(m);
+    solution(n, m, ans, input, pairs);
+    for(int i = 0; i < m; i++) {
+        std::cout << ans[i] << '\n';
+    }
+    return 0;
+}
+
+void solution(int n, int m, std::vector<int>& ans,
+              std::vector<int>& input, std::vector<std::pair<int, int>>& pairs) {
+    int father, a, b;
     std::vector<int>parent(n);
     std::vector<std::vector<int>>sons(n);
     for(int i = 0; i < n; i++) {
-        std::cin >> parent[i];
+        parent[i] = input[i];
         --parent[i];
         if(parent[i] == -1) father = i;
         else sons[parent[i]].push_back(i);
     }
+    int timer = 0;
     std::vector<int>tin(n);
     std::vector<int>tout(n);
     dfs(father, timer, tin, tout, parent, sons);
-    std::cin >> m;
     for(int i = 0; i < m; i++) {
-        std::cin >> a >> b;
-        --a;
-        --b;
-        if(tin[a] < tin[b] && tout[a] > tout[b]) std::cout << 1 << '\n';
-        else std::cout << 0 << '\n';
+        a = pairs[i].first - 1;
+        b = pairs[i].second - 1;
+        if(tin[a] < tin[b] && tout[a] > tout[b]) ans[i] = 1;
+        else ans[i] = 0;
     }
-    return 0;
 }
 
 void dfs(int v, int& timer, std::vector<int>& tin, std::vector<int>& tout,
